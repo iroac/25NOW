6807a87e18698a96557997f265f26d0ce12d0dbf
Vinicius Nogueira <vns5218@icloud.com>
2023-02-28 19:21:09 -0300

    new add todo method

--- src/App.js
+++ src/App.js
@@ -8,17 +8,44 @@ function App() {
   const [todogrup, setTodoGrup] = useState([])
   const [activeIndex, setActiveIndex] = useState('')
   const [clicked, setClicked] = useState(false)
+  const [addItems, SetAddItems] = useState('')
 
+
+  // I just pass most time of my day (02/28) to make this code. I was hard because at first I think 
+  // "I will have to add a item on an array inside an object on a database with multiples obj"
+  // I tried use the Title inicialy to acess that with the .findIndex obj method, but I forgot that every element on the json got have an id, so after a long time a notice it and put it.
+  // So then I tried to change the second paramet of addItem for the id, for this when we create the option list the value will be assign to the id,
+  // when we require that on select so here we use it to acess the id. after find the index with findIndex(this time it works cause I'm compare with the id not with index like I did first)
+  // Then we create a new varible and assign that to the todogrup so we'll not modify the status. I know't how but if we not do this the renderTodo apresents a error that can map. So I guess that state has to stay aways clean in all modification.
+  // so we push the item to the varible and set the TodoGrup for it, create a varible the represent the currently obj to be modified and put that as the second argument on the axios.put as know as payload.
+  const addItem = async (item, id) => {
+
+    const listIndex = todogrup.findIndex(list => list.id === id)
+    const newState = [...todogrup]
+    newState[listIndex].Itens.push(item)
+    setTodoGrup(newState)
+
+    const newItem = { ...newState[listIndex] }
+    await axios.put(`http://localhost:3000/todogroup/${id}`, newItem)
+  }
+
+
+  // All below is refering to the data fetch and the functions to change between menus edit/add
+
+  // Set the current item index that was clicked, with the index list and index item so just active the item to red
+  // Set the cliked state so the menus can change between then
   const handleClick = (index) => {
     setActiveIndex(index)
     setClicked(true)
   }
 
+  // A function that goes to Editinput as a props so when click on add we change to AddInput and unselect any active item
   const handleMenuClick = () => {
     setClicked(false)
     setActiveIndex('')
   }
 
+  // Maps to handle with the fetch to display currectly 
   const renderedLists = todogrup.map((data, listindex) => {
 
     let items = data.Itens
@@ -35,10 +62,12 @@ function App() {
     )
   })
 
+  // A map that take the Title to put on the selection as options at AddInput
   const optionsList = todogrup.map((todo) => {
-    return { value: todo.Title, label: todo.Title }
+    return { value: todo.id, label: todo.Title }
   })
 
+  // A fetch to take all list and itens from the database
   useEffect(() => {
     const fetchData = async () => {
       const data = await axios.get('http://localhost:3000/todogroup')
@@ -52,7 +81,7 @@ function App() {
       <h1 className="text-3xl text-red-600 font-bold underline">Let's start</h1>
     </div>
     <div className="flex flex-row justify-center pt-60">{renderedLists}</div>
-    {clicked ? <EditInput clicked={handleMenuClick} /> : <AddInput options={optionsList} />}
+    {clicked ? <EditInput clicked={handleMenuClick} /> : <AddInput add={addItem} options={optionsList} />}
   </div>
 }
 
--- src/components/AddInput.js
+++ src/components/AddInput.js
@@ -2,8 +2,21 @@
 import { useEffect, useState } from 'react'
 import Select from 'react-select'
 
-function AddInput({ options }) {
+function AddInput({ options, add }) {
     const [option, setOption] = useState([])
+    const [addList, setAddList] = useState(false)
+    const [addInput, setAddInput] = useState('')
+    const [selectedOption, setSelectedOption] = useState(null);
+
+    const handleAdd = () => {
+        add(addInput, selectedOption.value)
+        setSelectedOption(null)
+        setAddInput('')
+    }
+
+    const showInput = () => {
+        setAddList(!addList)
+    }
 
     useEffect(() => {
         setOption(options)
@@ -28,11 +41,11 @@ function AddInput({ options }) {
 
     return <div className="flex flex-row justify-center items-center mt-32" >
         <div className=" bg-red-600 w-auto h-2/6 px-2 rounded-full justify-start items-center flex flex-row">
-            <input className=" h-8 p-auto focus:outline-0 "></input>
-            <Select styles={customStyles} options={option} placeholder="choose a group"></Select>
-            <i className="ri-add-circle-fill text-5xl text-regal-white"></i>
-            <input className=" h-8 focus:outline-0"></input>
-            <i className="ri-folder-add-fill text-5xl text-regal-white"></i>
+            <input onChange={(e) => { setAddInput(e.target.value) }} value={addInput} className=" h-8 p-auto focus:outline-0 "></input>
+            <Select onChange={(option) => { setSelectedOption(option) }} value={selectedOption} styles={customStyles} options={option} placeholder="choose a group"></Select>
+            <i className="ri-add-circle-fill text-5xl text-regal-white" onClick={handleAdd}></i>
+            {addList ? <input className=" h-8 focus:outline-0"></input> : ''}
+            <i className="ri-folder-add-fill text-5xl text-regal-white" onClick={showInput}></i>
         </div>
     </div>
 }
