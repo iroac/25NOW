fc7483960e6a15c16098a1b0e59648e475a977df
Vinicius Nogueira <vns5218@icloud.com>
2023-07-19 15:22:33 -0300

    add https on urls

--- server/index.js
+++ server/index.js
@@ -39,7 +39,7 @@ app.use(cookieParser());
 const sessionConfig = { secret: 'wjdijfaWISomecrazyvaribleherefjaifaw', resave: false, saveUninitialized: true, cookie: { secure: false }, maxAge: 24 * 60 * 60 * 1000 }
 app.use(session(sessionConfig))
 // Cors config middleware
-app.use(cors({ origin: 'http://25-now-iroac.vercel.app', credentials: true })) // enable cookies and sessions across domains}));
+app.use(cors({ origin: 'https://25-now-iroac.vercel.app/', credentials: true })) // enable cookies and sessions across domains}));
 
 
 
