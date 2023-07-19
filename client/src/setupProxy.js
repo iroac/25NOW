a39d24f85c81cd491c04b11db97ef3866ce8a6b5
Vinicius Nogueira <vns5218@icloud.com>
2023-07-19 14:43:22 -0300

    fix api url

--- client/src/setupProxy.js
+++ client/src/setupProxy.js
@@ -4,7 +4,7 @@ module.exports = function (app) {
     app.use(
         '/api',
         createProxyMiddleware({
-            target: '25now-api.vercel.app',
+            target: 'http://25now-api.vercel.app',
             changeOrigin: true,
         })
     );
