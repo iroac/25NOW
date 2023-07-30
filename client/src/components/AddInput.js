24f06ee15634aba6347bbed9a9527f2fc498a442
Vinicius Nogueira <vns5218@icloud.com>
2023-07-30 12:46:15 -0300

    refactory AddInput components classes

--- client/src/components/AddInput.js
+++ client/src/components/AddInput.js
@@ -81,14 +81,14 @@ function AddInput({ options, add, addlist, restTime, onArchivedUpdate, doneItems
 
 
     return <div className="flex flex-row justify-center items-center mt-16 " >
-        <i className={`ri-history-line text-5xl pr-2 cursor-pointer  ${restTime ? 'text-green-600 hover:text-green-500' : 'text-red-600 hover:text-red-500 '}`} onClick={handleModelHistory} ></i>
-        <i className={`ri-archive-line text-5xl pr-2 cursor-pointer  ${restTime ? 'text-green-600 hover:text-green-500 ' : 'text-red-600 hover:text-red-500 '}`} onClick={handleModel} ></i>
-        <div className={`${restTime ? 'bg-green-600' : 'bg-red-600'} w-auto h-5/2  px-2 rounded-full justify-start items-center flex flex-row`}>
-            {!addList && <> <form onSubmit={(e) => handleAdd(e)} ><input onChange={(e) => { setAddInput(e.target.value) }} value={addInput} className="h-8 p-auto focus:outline-0 "></input></form>
-                <Select onChange={(option) => { setSelectedOption(option) }} value={selectedOption} className=" focus:outline-none " styles={customStyles} options={option} placeholder="choose a group"></Select>
-                <i className="ri-add-circle-fill cursor-pointer text-5xl text-regal-white hover:text-regal-contrast" onClick={handleAdd}></i> </>}
+        <i className={`ri-history-line text-2xl sm:text-5xl pr-2 cursor-pointer  ${restTime ? 'text-green-600 hover:text-green-500' : 'text-red-600 hover:text-red-500 '}`} onClick={handleModelHistory} ></i>
+        <i className={`ri-archive-line text-2xl sm:text-5xl  pr-2 cursor-pointer  ${restTime ? 'text-green-600 hover:text-green-500 ' : 'text-red-600 hover:text-red-500 '}`} onClick={handleModel} ></i>
+        <div className={`${restTime ? 'bg-green-600' : 'bg-red-600'} w-auto h-5/2  px-2 rounded-full justify-center items-center flex flex-row`}>
+            {!addList && <> <form onSubmit={(e) => handleAdd(e)} ><input onChange={(e) => { setAddInput(e.target.value) }} value={addInput} className="h-8 p-auto w-auto focus:outline-0 "></input></form>
+                <Select onChange={(option) => { setSelectedOption(option) }} value={selectedOption} className=" w-auto h-2/5  focus:outline-none " styles={customStyles} options={option} placeholder="choose a group"></Select>
+                <i className="ri-add-circle-fill cursor-pointer text-2xl sm:text-5xl text-regal-white hover:text-regal-contrast" onClick={handleAdd}></i> </>}
             {addList ? <form onSubmit={(e) => showInput(e)} > <input onChange={(e) => { setAddNewList(e.target.value) }} value={addNewList} className=" h-8 focus:outline-0"></input> </form> : ''}
-            <i className="ri-folder-add-fill cursor-pointer text-5xl text-regal-white hover:text-regal-contrast" onClick={showInput}></i>
+            <i className="ri-folder-add-fill cursor-pointer text-2xl sm:text-5xl text-regal-white hover:text-regal-contrast" onClick={showInput}></i>
         </div>
         {modal && <ArchiveList restTime={restTime} onArchivedUpdate={onArchivedUpdate} onClose={handleModel} />}
         {modalHistory && <HistoryItems restTime={restTime} onDeleteDone={onDeleteDone} onMoveDone={onMoveDone} doneItems={doneItems} onCloseHistory={handleModelHistory} />}
